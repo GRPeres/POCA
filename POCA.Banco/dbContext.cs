5de95cfe89d23f0faeafaab6ab8840801a818c7c
Gabriel <gr.peres@hotmail.com>
2025-03-19 12:10:49 -0300

    BancoScaffoldingTry

---
+++ POCA.Banco/dbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using MySql.EntityFrameworkCore;
+
+
+namespace POCA.Banco
+{
+    public class dbContext : DbContext
+    {
+        public dbContext(DbContextOptions<dbContext> options) : base(options) { }
+
+        // Defina suas DbSets aqui
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            }
+        }
+    }
+}
