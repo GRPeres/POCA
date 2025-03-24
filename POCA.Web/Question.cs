78c7400a7241821c58979de872f5f53814242f45
João Pedro Pogere <94464569+Pogeres@users.noreply.github.com>
2025-03-24 19:19:33 -0300

    Upload estatic question

---
+++ POCA.Web/Question.cs
@@ -0,0 +1,17 @@
+﻿namespace QuizApp.Models
+{
+    public class Question
+    {
+        public string QuestionText { get; set; } // Propriedade não anulável
+        public List<string> Options { get; set; } // Propriedade não anulável
+        public int CorrectAnswerIndex { get; set; }
+
+        // Construtor atualizado
+        public Question(string questionText, List<string> options, int correctAnswerIndex)
+        {
+            QuestionText = questionText ?? throw new ArgumentNullException(nameof(questionText));
+            Options = options ?? throw new ArgumentNullException(nameof(options));
+            CorrectAnswerIndex = correctAnswerIndex;
+        }
+    }
+}
