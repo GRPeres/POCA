7ecff045cd6e3e969ae474930e1883877201f044
Whitherhs <vicenzo.bessegatto@sempreceub.com>
2025-05-04 22:40:32 -0300

    Criação da Página de Login

---
+++ POCA.Web/Login.cs
@@ -0,0 +1,27 @@
+﻿namespace POCA.Web
+{
+    public class LoginService
+    {
+        private string ID;
+        private string Usuario;
+        private string Email;
+        private string Senha;
+
+        public LoginService(string id, string usuario, string email, string senha)
+        {
+            ID = id;
+            Usuario = usuario;
+            Email = email;
+            Senha = senha;
+        }
+
+        public bool AutenticarUsuario(string username, string password)
+        {
+            if ((username == Usuario || username == Email) && password == Senha)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
