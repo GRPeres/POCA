076a1bef2ec2c03a842a1d0eaf187f66c0c2a124
Gabriel <gr.peres@hotmail.com>
2025-05-09 21:47:16 -0300

    UI material design 2.0

---
+++ POCA.Web/Requests/QuestaoBatchImportRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public record QuestaoBatchImportRequest(List<QuestaoImportModel> Questoes);
+
+public record QuestaoImportModel
+{
+    public string EnunciadoQuestao { get; set; }
+    public string RespostaCertaQuestao { get; set; }
+    public string RespostaErrada1Questao { get; set; }
+    public string RespostaErrada2Questao { get; set; }
+    public string RespostaErrada3Questao { get; set; }
+    public string DificuldadeQuestao { get; set; }
+    public string TemaQuestao { get; set; }
+}
---
+++ POCA.Web/wwwroot/site.js
@@ -0,0 +1,21 @@
+﻿function downloadFile(baseUrl, fileName, contentType, content) {
+    const link = document.createElement('a');
+    link.href = `data:${contentType};base64,${content}`;
+    link.download = fileName;
+    link.click();
+}
+
+// Get dropped files
+function getDroppedFiles() {
+    return window.lastDroppedFiles;
+}
+
+// Setup global drop handler
+document.addEventListener('dragover', function (e) {
+    e.preventDefault();
+});
+
+document.addEventListener('drop', function (e) {
+    e.preventDefault();
+    window.lastDroppedFiles = e.dataTransfer.files;
+});
