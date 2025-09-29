89cdfe98aa78b2f55b4f16acd313a45e16b94ed6
Biribis <106186796+Biribis@users.noreply.github.com>
2025-09-29 17:27:02 -0300

    Response para o botao de sugerir ativ de revisao

---
+++ POCA.Web/Response/RespostaResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace POCA.API.Response
+{
+    public class RespostaResponse
+    {
+        public int IdResposta { get; set; }
+        public string FinalResposta { get; set; } = "";
+
+        public int IdAtividade { get; set; }
+        public int IdAluno { get; set; }
+        public int IdQuestao { get; set; }
+    }
+}
